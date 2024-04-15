b9e65d439f6f3239a17016cc87e0008e0f352a13
SkopovaEva <st72711@upce.cz>
2024-04-16 00:29:31 +0200

    handle exception

--- src/main/java/com/example/nnpia_semestralka/Controller/ProductController.java
+++ src/main/java/com/example/nnpia_semestralka/Controller/ProductController.java
@@ -6,6 +6,7 @@ import com.example.nnpia_semestralka.Repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,11 @@ public class ProductController {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @ExceptionHandler(RuntimeException.class)
+    public String handleException() {
+        return "error";
+    }
 
     @GetMapping("/")
     public String showProducts(Model model) {
