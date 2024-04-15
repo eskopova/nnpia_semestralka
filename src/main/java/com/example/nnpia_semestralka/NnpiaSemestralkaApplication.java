97865fe27d3c8296b54afed31bed0162a126d256
SkopovaEva <st72711@upce.cz>
2024-04-15 22:38:54 +0200

    init

---
+++ src/main/java/com/example/nnpia_semestralka/NnpiaSemestralkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.nnpia_semestralka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnpiaSemestralkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NnpiaSemestralkaApplication.class, args);
+	}
+
+}
