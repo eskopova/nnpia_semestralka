506c7e9571baa52ad88f2346503bfc29e22de7cf
SkopovaEva <st72711@upce.cz>
2024-04-16 00:22:43 +0200

    add product controller

---
+++ src/main/java/com/example/nnpia_semestralka/Controller/ProductController.java
@@ -0,0 +1,45 @@
+package com.example.nnpia_semestralka.Controller;
+
+import com.example.nnpia_semestralka.Dto.AddOrEditProductDto;
+import com.example.nnpia_semestralka.Entity.Product;
+import com.example.nnpia_semestralka.Repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("/")
+    public String showProducts(Model model) {
+        model.addAttribute("productList", productRepository.findAll());
+        return "product-list";
+    }
+
+    @GetMapping(value = {"/product-form", "/product-form/{id}"})
+    public String addOrEditProduct(@PathVariable(required = false) Long id, Model model) {
+        if (id != null) {
+            Product product = productRepository.findById(id).orElse(new Product());
+            model.addAttribute("product", product);
+        } else {
+            model.addAttribute("product", new AddOrEditProductDto());
+        }
+        return "product-form";
+    }
+
+    @PostMapping("product-form-process")
+    public String addOrEditProductProcess(AddOrEditProductDto addOrEditProductDto) {
+        Product product = new Product();
+        product.setId(addOrEditProductDto.getId());
+        product.setProductName(addOrEditProductDto.getProductName());
+
+        productRepository.save(product);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/example/nnpia_semestralka/Dto/AddOrEditProductDto.java
@@ -0,0 +1,11 @@
+package com.example.nnpia_semestralka.Dto;
+
+import lombok.Data;
+
+@Data
+public class AddOrEditProductDto {
+
+    private Long id;
+
+    private String productName;
+}
