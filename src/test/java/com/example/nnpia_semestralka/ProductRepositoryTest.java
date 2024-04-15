de9413c638ad5cfe0072e1db60ef07c4a083625f
SkopovaEva <st72711@upce.cz>
2024-04-16 00:02:27 +0200

    repository test

--- src/test/java/com/example/nnpia_semestralka/ProductRepositoryTest.java
+++ src/test/java/com/example/nnpia_semestralka/ProductRepositoryTest.java
@@ -3,10 +3,15 @@ package com.example.nnpia_semestralka;
 import com.example.nnpia_semestralka.Entity.Product;
 import com.example.nnpia_semestralka.Repository.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase
 public class ProductRepositoryTest {
 
     @Autowired
