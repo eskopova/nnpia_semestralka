b7a9725b1bcf5f931214cf7c4fe0c25242046af1
SkopovaEva <st72711@upce.cz>
2024-04-16 09:41:26 +0200

    adding and removing items from shopping cart

--- src/main/java/com/example/nnpia_semestralka/Controller/ShoppingCartController.java
+++ src/main/java/com/example/nnpia_semestralka/Controller/ShoppingCartController.java
@@ -1,5 +1,6 @@
 package com.example.nnpia_semestralka.Controller;
 
+import com.example.nnpia_semestralka.Service.ShoppingCartService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,20 +9,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class ShoppingCartController {
 
+    private final ShoppingCartService shoppingCartService;
+
+    public ShoppingCartController(ShoppingCartService shoppingCartService) {
+        this.shoppingCartService = shoppingCartService;
+    }
+
     @GetMapping("/shopping-cart-add/{id}")
     public String shoppingCartAdd(@PathVariable Long id, Model model) {
-
-        return "redirect:/shopping-cart-add";
+        shoppingCartService.add(id);
+        return "redirect:/shopping-cart";
     }
 
     @GetMapping("/shopping-cart-remove/{id}")
     public String shoppingCartRemove(@PathVariable Long id, Model model) {
-
-        return "redirect:/shopping-cart-add";
+        shoppingCartService.remove(id);
+        return "redirect:/shopping-cart";
     }
 
     @GetMapping("/shopping-cart")
-    public String showShoppingCart() {
-
+    public String showShoppingCart(Model model) {
+        model.addAttribute("shoppingCart", shoppingCartService.getCart());
+        return "shopping-cart";
     }
 }
